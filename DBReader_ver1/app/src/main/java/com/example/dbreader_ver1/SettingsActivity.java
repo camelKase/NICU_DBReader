d81394c75aa0e68c1c525a7b2b464a32e40bec88
Noah Lone <noah.lonen@gmail.com>
2019-04-10 18:30:05 -0400

    Added the following: API Min is 25, Pause/Listen button, adjust db alert level (does not actually update), alert message

---
+++ DBReader_ver1/app/src/main/java/com/example/dbreader_ver1/SettingsActivity.java
@@ -0,0 +1,30 @@
+package com.example.dbreader_ver1;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        backButton();
+    }
+
+    private void backButton() {
+        Button ibList = (Button) findViewById(R.id.button4);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+}
